a9f7ab97111bb6b7b4f227b8c810ce309f1d3c49
wildfireOfMine <iamtheanguera@gmail.com>
2026-04-25 22:29:24 +0200

    Added an Error404 site

---
+++ src/views/Error404.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Error404 = () => {
+  return (
+    <Box>
+      Site not found
+    </Box>
+  )
+}
+
+export default Error404
