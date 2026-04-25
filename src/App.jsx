aca4a0b23459b8db9803d509481afd6088f9c293
wildfireOfMine <iamtheanguera@gmail.com>
2026-04-25 22:28:42 +0200

    Added React-Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AppRoute from './routes/AppRoute'
 import Navbar from './components/Navbar'
+import { ToastContainer } from 'react-toastify'
 
 const App = () => {
   return (
     <>
       <Navbar/>
       <AppRoute/>
+      <ToastContainer/>
     </>
   )
 }
